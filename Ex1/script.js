1b789b4387c948c591b7acdb2d8dc746b553ccf3
steeve0304 <zychsteeve4@gmail.com>
2023-06-27 14:35:46 +0200

    ajout exo 1

---
+++ Ex1/script.js
@@ -0,0 +1,23 @@
+
+
+    function generate() {
+        fetch("becode.json")
+            .then((response) => response.json())
+            .then(json => generateRules(json))
+            .catch((error) => {
+                console.log("There was an error!", error);
+            });
+    }
+    
+    function generateRules(json) {
+        let ul = document.createElement('ul');
+        document.body.appendChild(ul);
+    
+    
+        for (let txt of json){
+            const li = document.createElement("li");
+            li.innerText = txt;
+            ul.appendChild(li);
+        }
+    
+    }
